808e6143df35df13039b2d08eaab218fa16b09dc
Marc Sigonius <marcsigonius@n132-p20.kthopen.kth.se>
2017-02-24 11:32:23 +0100

    initial commit

---
+++ src/FindMembership.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class FindMembership {
+	public FindMembership() {
+	}
+	//(i) Sorting
+	//@ requires list.length > 0;
+	//@ ensures (\forall int i; 0 <= i && i < \result.length-1; \result[i]<\result[i+1]);
+	public int[] sort(int[] list){
+		int length = list.length;
+
+		int newLength;
+		while (length != 0) {
+			newLength = 0;
+			for (int i = 1; i < length; i++) {
+				if (list[i - 1] > list[i]) {
+					int tmp = list[i];
+					list[i] = list[i-1];
+					list[i-1] = tmp;			
+				}
+			}
+			length = newLength;
+		}
+		return list;
+	}
+	
+
+	// (ii) %Searching
+	//@requires list.length > 0;
+	//@ensures list[\result] == key | \result == -1;
+	public int search(int[] list, int key){return 1;}
+
+	//(iii) %Membership
+	// requires list.length > 0;
+	// ensures (\exists int i; 0 <= i && i < list.length-1; list[i]==key) && \result | !(\exists int i; 0 <= i && i < list.length-1; list[i]==key) && !\result;
+	public int membership(int[] list, int key){
+		int x,l,r;
+		l=1;
+		r=list.length;
+		x=(l+r)/2;
+		while(key!=list[x] || l>r){
+			if(key<list[x]){
+				r = x-1;
+			}
+			else{
+				l=x+1;
+			}
+		}
+		if(list[x] == key) return x;
+		else return -1;
+	}
+	
+	//(iv) %Binary search
+	//@requires list.length > 0;
+	//@ensures key == list[\result] | \result == -1;
+	public int binary_search(int[] list, int key){
+		list = sort(list);
+		return membership(list,key);
+	}
+	
+	public static void main(String[] args) {
+		new FindMembership();
+	}
+}
